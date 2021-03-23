47d0c7846ed755e07dad9dc872400ed8fced8c52
majik <me@yamajik.com>
2021-03-23 17:47:18 +0800

    actor invoke

--- examples/fn/exec/v2.py
+++ examples/fn/exec/v2.py
@@ -1,9 +1,18 @@
-from kess import Actor, actormethod
+from kess import Actor, actormethod, ActorProxy, actorinvoke
+
 
 class ExecV2(Actor):
-    @actormethod("test")
-    async def test(self, data: object) -> object:
-        return {"test": "data", **data}
+    @actormethod("apiv1")
+    async def apiv1(self, data: object) -> object:
+        return {"data": "apiv1", **await ActorProxy(ExecV2).apiv3(data)}
 
-    async def testv(self, data: object) -> object:
-        return {"testv": "data", **data}
+    @actormethod("apiv2")
+    async def apiv2(self, data: object) -> object:
+        return {"data": "apiv2", **await ActorProxy(name="ExecV2").invoke("apiv3", data=data)}
+
+    @actormethod("apiv3")
+    async def apiv3(self, data: object) -> object:
+        return {"data": "apiv3", **data}
+
+    async def notapi(self, data: object) -> object:
+        return {"data": "notapi", **data}
--- kess/__init__.py
+++ kess/__init__.py
@@ -1,4 +1,11 @@
 from kess.app import App
 from kess.function import Function
 from kess.exceptions import *
-from kess.actor import Actor, Remindable, method as actormethod
+from kess.actor import (
+    Actor,
+    Remindable,
+    method as actormethod,
+    create_interface as ActorInterface,
+    create_proxy as ActorProxy,
+    invoke as actorinvoke,
+)
